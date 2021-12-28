8e3a87e7586519561f158ee305dccd9642d3147d
José Victor D. Fernandes <josevictordf@gmail.com>
2021-12-28 16:25:50 -0300

    Initial Commit

---
+++ I am Rich Course App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich Course App
+//
+//  Created by Jose Victor Fernandes on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
